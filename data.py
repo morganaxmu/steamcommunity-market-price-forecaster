e3f910068e16507d6377f71991bb2af58320b76c
Billy Huang <42191587+morganaxmu@users.noreply.github.com>
2019-12-27 19:50:52 +0800

    19.12.27.2

---
+++ data.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  data.py
+#  
+#  Copyright 2019 billy huang <billy huang@DESKTOP-77CQ0AV>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+import json
+filename = 'Gems.json'
+with open(filename) as f:
+   data = json.loads(str(f), strict=False)
+   print(data)
